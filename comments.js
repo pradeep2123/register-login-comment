04e72cf0d54cd9c767baddd878ce4aa30aae15a0
pradeep2123 <35168431+pradeep2123@users.noreply.github.com>
2018-05-07 04:00:26 -0700

    Update and rename comments.js to empty

--- comments.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema,
-ObjectId = Schema.ObjectId;
-var Comments = new Schema({
-comment :String,    
-postId :String
-});
-module.exports = mongoose.model('Comments', Comments);
-
