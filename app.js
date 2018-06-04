fa4965b5af2405fcaed8df63af7aa0f19f54eabc
pradeep2123 <35168431+pradeep2123@users.noreply.github.com>
2018-06-04 06:37:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -54,50 +54,6 @@ app.get('/login',(req,res)=>{
   res.render(path.join(__dirname,'/views/login.ejs'));     
 })  
 
-// usernames which are currently connected in the room
-var usernames = {};
-
-var rooms = ['room1','room2','room3'];
-
-io.sockets.on('connection', function (socket) {
-
-	socket.on('adduser', function(username){
-    
-    socket.username = username;
-
-    socket.room = 'room1';
-				usernames[username] = username;
-				socket.join('room1');
-				socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-				socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-		socket.emit('updaterooms', rooms, 'room1');
- 	});
-
-	
-	socket.on('sendchat', function (data) {
-	
-		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
-	});
-
-	socket.on('switchRoom', function(newroom){
-			socket.leave(socket.room);
-			socket.join(newroom);
-
-      socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-				socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
-				socket.room = newroom;
-  
-        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-		socket.emit('updaterooms', rooms, newroom);
-	});
-
-		socket.on('disconnect', function(){
-			delete usernames[socket.username];
-		io.sockets.emit('updateusers', usernames);
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-		socket.leave(socket.room);
-	});
-});
 
 app.post ('/signup',function(req,res,next){
   console.log(req.body)
@@ -168,7 +124,7 @@ app.get('/confirmation/:token', (req,res,next)=>{
      
             if (err) 
             { 
-              return res.render(path.join(__dirname,'/views/error.ejs'));
+              return res.render(path.join(__dirname,'/views/error.ejs'),{message: "Not saved to database "});
             }
             else
             {
@@ -202,7 +158,7 @@ app.get('/confirmation/:token', (req,res,next)=>{
                 }
               }   
                 else 
-                res.render(path.join(__dirname,'/views/error.ejs')) 
+                res.render(path.join(__dirname,'/views/error.ejs'),{message:"not a authorization user, go and verifiy a link"}) 
             
             
          
